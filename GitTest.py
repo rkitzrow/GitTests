364a34d61e92968074b702d68fed63b4cbf0783f
Ryne Kitzrow <rynekitzrow@gmail.com>
2018-09-22 13:25:10 -0400

    Initial commit

---
+++ GitTest.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/user/<int:post_num>')
+# here I set the url to include a positive integer which will be manipulated later in the code
+def show_num(post_num):
+    return 'The user number multiplied by 100 is %d' % (post_num*100)
+
+#set port to 8003
+if __name__ == "__main__":
+    app.run(port=8003)
